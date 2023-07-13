1fe9d45401db92d3a68bee626b309da693a530c1
S Soundarya <soundarya.s@tvsnext.com>
2023-07-13 21:04:16 +0530

    [Soundarya] Add: winning position condition.

--- SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SnakeAndLadder
 {
-    public class Operations
+    public class SnakeAndLadder
     {
         const int NO_PLAY = 0, LADDER = 1, SNAKE = 2, WINNING_POSITION = 100, STARTING_POSITION = 0;
         int position = 0;
