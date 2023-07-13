5f01b08c9c5ce3cd488d5896286175d13ef23cb8
S Soundarya <soundarya.s@tvsnext.com>
2023-07-13 20:21:25 +0530

    [Soundarya] Add: folder structure.

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and ladder Program");
+        }
+    }
+}
